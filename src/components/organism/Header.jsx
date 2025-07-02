c93b14cfcf8aa0e51460076912cb6ad978298eaa
M. Rizky Aditya Utama <mrizkyadityautama@gmail.com>
2025-07-02 13:44:27 +0700

    Small fix

--- src/components/organism/Header.jsx
+++ src/components/organism/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import MRAU from '../../assets/MRAU Logo.svg'
+import MRAU from '../../assets/MRAU-Logo.svg'
 
 const navLink = 'transition-transform hover:scale-110'
 
