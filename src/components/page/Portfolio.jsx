a1f90b6729e70278d6d7c1206fa3b18005c4fa17
M. Rizky Aditya Utama <mrizkyadityautama@gmail.com>
2025-07-02 23:24:31 +0700

    Adding some content inside Portfolio page

--- src/components/page/Portfolio.jsx
+++ src/components/page/Portfolio.jsx
@@ -4,15 +4,20 @@ import Footer from "../organism/Footer"
 import Header from "../organism/Header"
 import RizCod from '../../assets/Rizky-Code.jpg'
 
+import ToDo from '../../assets/ToDoList.svg'
+import ChillV1 from '../../assets/ChillV1.svg'
+
 
 const Project = () => {
   return (
     <>
         <Header/>
-        <Main className='flex justify-center items-center gap-6'>
-            <ProjectCard text='To Do List' img={RizCod}>Selling various Premium Applications such as video streaming, music, design, editing, education, and more.</ProjectCard>
-            <ProjectCard text='To Do List' img={RizCod}></ProjectCard>
-            <ProjectCard text='To Do List' img={RizCod}></ProjectCard>
+        <Main className='flex flex-col justify-center items-center gap-6'>
+          <h1>Portfolio</h1>
+          <section className='flex justify-center items-center gap-6'>
+            <ProjectCard text='Chill - Video Stream' img={ChillV1} link='https://github.com/rzkditya/Chill-Static' techStack='HTML, CSS'>A Netflix-style video streaming app. Features a premium-style UI showcasing movies, shows, and more. All in a responsive layout.</ProjectCard>
+            <ProjectCard text='Dit To Do Dit' img={ToDo} link='https://github.com/rzkditya/DitToDoDit' techStack='HTML, CSS, JavaScript'>A clean and responsive To-Do list app. Manage tasks easily across devices with a minimalist design and local storage support.</ProjectCard>
+          </section>
         </Main>
         <Footer/>
     </>
